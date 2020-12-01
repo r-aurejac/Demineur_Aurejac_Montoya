9b072b59a652f978f79a9ea37b26a5d62835aca0
raphael <raphael.aurejac@gmail.com>
2020-12-01 17:04:47 +0100

    Merge branch 'master' of C:\Users\raphael\AndroidStudioProjects\Demineur_Aurejac_Montoya with conflicts.

---
+++ app/src/main/java/com/example/demineur_aurejac_montoya/CaseView.java
@@ -0,0 +1,29 @@
+package com.example.demineur_aurejac_montoya;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+public class CaseView extends View {
+    public CaseView(Context context) {
+        super(context);
+    }
+
+    public CaseView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public CaseView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public CaseView(Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+    private void init(@Nullable AttributeSet set){
+
+    }
+}
