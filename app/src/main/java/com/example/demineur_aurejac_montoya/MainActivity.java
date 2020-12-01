ea196ff29623a8142b73eeb5d45f0c4ea16ba804
raphael <raphael.aurejac@gmail.com>
2020-12-01 17:14:10 +0100

    target version 30

--- app/src/main/java/com/example/demineur_aurejac_montoya/MainActivity.java
+++ app/src/main/java/com/example/demineur_aurejac_montoya/MainActivity.java
@@ -3,12 +3,16 @@ package com.example.demineur_aurejac_montoya;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    HexagonMaskView hex1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
